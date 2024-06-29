75a983f593f72b33644d5e8daf5527a71b95b1ad
Joshua Bischoff <joshuabischoff1@gmail.com>
2024-06-29 18:02:19 -0400

    Created a login method on the SessionsController that validates user credentials and creates a new user session.

--- app/controllers/sessions_controller.ts
+++ app/controllers/sessions_controller.ts
@@ -1,4 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import { loginWithEmailValidator } from '#validators/login'
+import { loginWithUsernameValidator } from '#validators/login'
 
 export default class SessionsController {
   /**
@@ -6,5 +9,32 @@ export default class SessionsController {
    */
   public async show({ inertia }: HttpContext) {
     return inertia.render('auth/login')
+  }
+
+  /**
+   * 
+   * Validates sent data,
+   * Checks send login information against the database,
+   * Creates a new user session if the user credentials are correct
+   * 
+   * TODO: Redirect the logged in user to a dashboard home page
+   */
+  public async login({ request, auth }: HttpContext) {
+    const data = request.all()
+    if (data.username) {
+      const payload = await request.validateUsing(loginWithUsernameValidator)
+      const { username, password } = payload
+      const user = await User.verifyCredentials(username, password)
+      await auth.use('web').login(user)
+      console.log("Logged In")
+    } else if (data.email) {
+      const payload = await request.validateUsing(loginWithEmailValidator)
+      const { email, password } = payload
+      const user = await User.verifyCredentials(email, password)
+      await auth.use('web').login(user)
+      console.log("Logged In")
+    } else {
+      console.log("Something went wrong, please try again.")
+    }
   }
 }
