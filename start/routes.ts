5c7ba6c8d27ae7b69be289b6985605c8ba587894
Joshua Bischoff <joshuabischoff1@gmail.com>
2024-07-11 12:26:19 -0400

    Updated npm packages and added login page route.

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 const RegistrationsController = () => import('#controllers/registrations_controller')
+const SessionsController = () => import('#controllers/sessions_controller')
 
 router.on('/').renderInertia('home', { version: 6 })
 
@@ -16,4 +17,5 @@ router.on('/').renderInertia('home', { version: 6 })
 router.group(() => {
   router.get('/register', [RegistrationsController, 'show']).as('register.show')
   router.post('/register', [RegistrationsController, 'register']).as('register.submit')
+  router.get('/login', [SessionsController, 'show']).as('login.show')
 })
