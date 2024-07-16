c63f98e1c9862154269434647c0bc7f67ec5bb29
Joshua Bischoff <joshuabischoff1@gmail.com>
2024-07-16 15:56:36 -0400

    Added a Head component to the age restriction error page.

--- inertia/pages/errors/not_allowed/age_restriction.tsx
+++ inertia/pages/errors/not_allowed/age_restriction.tsx
@@ -1,6 +1,9 @@
+import { Head } from "@inertiajs/react"
+
 export default function AgeRestriction() {
     return (
         <div>
+            <Head title="Age Restriction" />
             <h1>You are not old enough to register an account!</h1>
             <p>In order to create a household, you must be at least 18 years old.</p>
             <p>If you are trying to joing a house hold, please try this link instead [link for minors will go here]</p>
