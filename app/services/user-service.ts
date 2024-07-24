fe2d837a7300b0fc510f7e89cbb9c56af557c056
Joshua Bischoff <joshuabischoff1@gmail.com>
2024-07-24 16:49:40 -0400

    Removed redundant code from User service.

--- app/services/user-service.ts
+++ app/services/user-service.ts
@@ -10,10 +10,6 @@ class UserService {
         const calculatedAge = AgeService.calculateAge(user.dateOfBirth)
         return calculatedAge >= 18
     }
-
-    public calculateAge(user: User) {
-        return AgeService.calculateAge(user.dateOfBirth)
-    }
 }
 
 export default new UserService
